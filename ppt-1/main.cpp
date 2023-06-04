b7cf32e57b461e13d13f128f505abf76f01658cd
tzorake <te.a4@yandex.ru>
2023-06-04 23:42:18 +0400

    Update .png

--- ppt-1/main.cpp
+++ ppt-1/main.cpp
@@ -5,7 +5,7 @@
 #include "matrix_helper.hpp"
 #include "text_table.hpp"
 
-double sequentialAlgorithm(int N, bool verbose = false)
+double measure_single(int N, bool verbose = false)
 {
     omp_set_num_threads(1);
     omp_set_schedule(omp_sched_static, 0);
@@ -24,8 +24,7 @@ double sequentialAlgorithm(int N, bool verbose = false)
 
     double end = omp_get_wtime();
 
-    if (verbose)
-    {
+    if (verbose) {
         std::cout << "A = \n" << A << std::endl;
         std::cout << "B = \n" << B << std::endl;
         std::cout << "X = \n" << X << std::endl;
@@ -39,16 +38,14 @@ double sequentialAlgorithm(int N, bool verbose = false)
     return elapsed;
 }
 
-double multiThreadedAlgorithm(int N, int M, bool useStatic = true, bool verbose = false)
+double measure_multi(int N, int M, bool useStatic = true, bool verbose = false)
 {
     omp_set_num_threads(M);
 
-    if (useStatic)
-    {
+    if (useStatic) {
         omp_set_schedule(omp_sched_static, 0);
     }
-    else
-    {
+    else {
         omp_set_schedule(omp_sched_dynamic, 0);
     }
 
@@ -66,8 +63,7 @@ double multiThreadedAlgorithm(int N, int M, bool useStatic = true, bool verbose 
 
     double end = omp_get_wtime();
 
-    if (verbose)
-    {
+    if (verbose) {
         std::cout << "A = \n" << A << std::endl;
         std::cout << "B = \n" << B << std::endl;
         std::cout << "X = \n" << X << std::endl;
@@ -81,7 +77,54 @@ double multiThreadedAlgorithm(int N, int M, bool useStatic = true, bool verbose 
     return elapsed;
 }
 
-void testAlgorithms(bool useStatic = true)
+double measure_multi_complex(int N, int M, int k, bool useStatic = true, bool verbose = false)
+{
+    omp_set_num_threads(M);
+
+    if (useStatic) {
+        omp_set_schedule(omp_sched_static, 0);
+    }
+    else {
+        omp_set_schedule(omp_sched_dynamic, 0);
+    }
+
+    MatrixHelper::setSeed(3);
+
+    MatrixD A = MatrixHelper::randomNonZeroDeterminantMatrix(N);
+    MatrixD B = MatrixHelper::randomMatrix(N, 1);
+
+    MatrixD C = MatrixHelper::randomNonZeroDeterminantMatrix(k);
+
+    double start = omp_get_wtime();
+
+    double A_det = A.determinant();
+    MatrixD A_adj = A.adjugate();
+    MatrixD A_inv = A_adj / A_det;
+
+    for (int i = 0; i < k; ++i) {
+        MatrixD D = C.copy();
+        C *= D * D.adjugate() / D.determinant();
+    }
+
+    MatrixD X = A_inv * B;
+
+    double end = omp_get_wtime();
+
+    if (verbose) {
+        std::cout << "A = \n" << A << std::endl;
+        std::cout << "B = \n" << B << std::endl;
+        std::cout << "X = \n" << X << std::endl;
+        std::cout << "B = A * X = \n" << A * X << std::endl;
+    }
+
+    double elapsed = end - start;
+
+    std::cout << "Elapsed time: " << elapsed << "s." << std::endl;
+
+    return elapsed;
+}
+
+void measure_using_schedule(bool useStatic = true)
 {
     TextTable t( '-', '|', '+' );
 
@@ -92,40 +135,34 @@ void testAlgorithms(bool useStatic = true)
     t.add( "E_p" );
     t.endOfRow();
 
-    std::vector<int> Ns { 7, 8, 9 };
-    std::vector<int> Ms { 2, 4, 8, 16 };
+    std::vector<int> Ns { 8, 9, 10 };
+    std::vector<int> Ms { 2, 3, 4, 5 };
 
     std::vector<double> seqValues;
 
-    for (size_t i = 0; i < Ns.size(); ++i)
-    {
+    for (size_t i = 0; i < Ns.size(); ++i) {
         int N = Ns[i];
 
-        double value = sequentialAlgorithm(N);
+        double value = measure_single(N);
 
         seqValues.push_back(value);
     }
 
-
     std::vector<std::vector<double>> mulValues(Ns.size());
 
-    for (int i = 0; i < Ns.size(); ++i)
-    {
-        for (int j = 0; j < Ms.size(); ++j)
-        {
+    for (int i = 0; i < Ns.size(); ++i) {
+        for (int j = 0; j < Ms.size(); ++j) {
             int N = Ns[i];
             int M = Ms[j];
 
-            double value = multiThreadedAlgorithm(N, M, useStatic);
+            double value = measure_multi(N, M, useStatic);
 
             mulValues[i].push_back(value);
         }
     }
 
-    for (int i = 0; i < Ns.size(); ++i)
-    {
-        for (int j = 0; j < Ms.size(); ++j)
-        {
+    for (int i = 0; i < Ns.size(); ++i) {
+        for (int j = 0; j < Ms.size(); ++j) {
             t.add( std::to_string( Ns[i] ) );
             t.add( std::to_string( Ms[j] ) );
             t.add( std::to_string( seqValues[i] ) );
@@ -138,20 +175,85 @@ void testAlgorithms(bool useStatic = true)
     std::cout << t;
 }
 
-void testAlgorithmsSchedules()
+void measure_using_schedule_complex()
 {
-    testAlgorithms(true);
-    testAlgorithms(false);
+    TextTable t( '-', '|', '+' );
+
+    t.add( "N" );
+    t.add( "M" );
+    t.add( "K" );
+    t.add( "T_1" );
+    t.add( "T_p" );
+    t.add( "E_p" );
+    t.endOfRow();
+
+    std::vector<int> Ns { 8, 9, 10 };
+    std::vector<int> Ms { 2, 3, 4, 5 };
+    std::vector<int> Ks { 8, 9 };
+
+    std::vector<double> seqValues;
+
+    for (size_t i = 0; i < Ns.size(); ++i) {
+        int N = Ns[i];
+
+        double value = measure_single(N);
+
+        seqValues.push_back(value);
+    }
+
+    std::vector<std::vector<double>> mulValues(Ns.size());
+
+    for (int i = 0; i < Ns.size(); ++i) {
+        for (int j = 0; j < Ms.size(); ++j) {
+            for (int k = 0; k < Ks.size(); ++k) {
+                    int N = Ns[i];
+                    int M = Ms[j];
+                    int K = Ks[k];
+
+                    double value = measure_multi_complex(N, M, K);
+
+                    mulValues[i].push_back(value);
+            }
+        }
+    }
+
+    for (int i = 0; i < Ns.size(); ++i) {
+        for (int j = 0; j < Ms.size(); ++j) {
+            for (int k = 0; k < Ks.size(); ++k) {
+                t.add( std::to_string( Ns[i] ) );
+                t.add( std::to_string( Ms[j] ) );
+                t.add( std::to_string( Ks[k] ) );
+                t.add( std::to_string( seqValues[i] ) );
+                t.add( std::to_string( mulValues[i][j*Ks.size() + k] ) );
+                t.add( std::to_string( seqValues[i] / (Ms[j] * mulValues[i][j + k]) ) );
+                t.endOfRow();
+            }
+        }
+    }
+
+    std::cout << t;
+}
+
+void compare_schedules()
+{
+    measure_using_schedule(true);
+    measure_using_schedule(false);
 }
 
 int main()
 {
-    sequentialAlgorithm(7, true);
-    multiThreadedAlgorithm(7, 8, true, true);
+    // UNCOMMENT IF WANT TO CHECK MEASUREMENT FOR SINGLETHREADED
+    // measure_single(10, true);
 
-    testAlgorithms();
+    // UNCOMMENT IF WANT TO CHECK MEASUREMENT FOR MULTITHREADED
+    // measure_multi(10, 4, true, true);
 
-    testAlgorithmsSchedules();
+    // UNCOMMENT IF WANT TO CHECK MEASUREMENT COMPARISON FOR SINGLETHREADED AND MULTITHREADED
+    // measure_using_schedule();
+
+    // measure_using_schedule_complex();
+
+    compare_schedules();
 
     return 0;
 }
